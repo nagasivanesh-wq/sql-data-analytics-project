ac0f8f5dd7b3cb920053386982dd522534782a0f
nagasivanesh-wq <nagasivanesh@gmail.com>
2026-09-15 20:46:29 +0530

    Add files via upload

---
+++ scripts/02_dimensions_exploration.sql
@@ -0,0 +1,12 @@
+SELECT DISTINCT 
+    country 
+FROM gold.dim_customers
+ORDER BY country;
+
+-- Retrieve a list of unique categories, subcategories, and products
+SELECT DISTINCT 
+    category, 
+    subcategory, 
+    product_name 
+FROM gold.dim_products
+ORDER BY category, subcategory, product_name;
